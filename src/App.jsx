6bde7777c9121a99a3c8b6e1a498217bb2287dac
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-12-03 22:47:07 -0800

    Add code-splitting for Details and SearchParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import AdoptedPetContext from "./AdoptedPetContext";
 import Intermediate from "./intermediate/Intermediate";
 import Home from "./intermediate/routes/Home";
@@ -13,6 +11,13 @@ import UseMemo from "./intermediate/routes/UseMemo";
 import UseReducer from "./intermediate/routes/UseReducer";
 import UseLayoutEffect from "./intermediate/routes/UseLayoutEffect";
 import UseId from "./intermediate/routes/UseId";
+
+// Code-splitting. Only load these modules when the user actually navigates to
+// these routes. Use `lazy` to do the import; Vite notices this, and splits them
+// out into separate bundles. Use Suspense later on to render a fallback element
+// while these bundles are downloading.
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -36,27 +41,38 @@ const App = () => {
       <BrowserRouter>
         <AdoptedPetContext.Provider value={adoptedPet}>
           <QueryClientProvider client={queryClient}>
-            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-              <Link className="text-6xl text-white hover:text-gray-200" to="/">
-                Adopt Me!
-              </Link>
-            </header>
-            <nav>
-              <Link to="/intermediate">Intermediate React v5</Link>
-            </nav>
-            <Routes>
-              <Route path="/intermediate" element={<Intermediate />}>
-                <Route path="" element={<Home />} />
-                <Route path="useReducer" element={<UseReducer />} />
-                <Route path="useMemo" element={<UseMemo />} />
-                <Route path="useCallback" element={<UseCallback />} />
-                <Route path="useRef" element={<UseRef />} />
-                <Route path="useLayoutEffect" element={<UseLayoutEffect />} />
-                <Route path="useId" element={<UseId />} />
-              </Route>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
+            <Suspense
+              fallback={
+                <div className="loading-pane">
+                  <h2 className="loader">🐶</h2>
+                </div>
+              }
+            >
+              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+                <Link
+                  className="text-6xl text-white hover:text-gray-200"
+                  to="/"
+                >
+                  Adopt Me!
+                </Link>
+              </header>
+              <nav>
+                <Link to="/intermediate">Intermediate React v5</Link>
+              </nav>
+              <Routes>
+                <Route path="/intermediate" element={<Intermediate />}>
+                  <Route path="" element={<Home />} />
+                  <Route path="useReducer" element={<UseReducer />} />
+                  <Route path="useMemo" element={<UseMemo />} />
+                  <Route path="useCallback" element={<UseCallback />} />
+                  <Route path="useRef" element={<UseRef />} />
+                  <Route path="useLayoutEffect" element={<UseLayoutEffect />} />
+                  <Route path="useId" element={<UseId />} />
+                </Route>
+                <Route path="/details/:id" element={<Details />} />
+                <Route path="/" element={<SearchParams />} />
+              </Routes>
+            </Suspense>
           </QueryClientProvider>
         </AdoptedPetContext.Provider>
       </BrowserRouter>
