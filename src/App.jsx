fac5b93ec05ba0b47e909566b1e83d6572b79b27
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-12-05 09:48:06 -0800

    Put BrowserRouter and App inside ClientApp to make it the new entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense, useState } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AdoptedPetContext from "./AdoptedPetContext";
 import Hooks from "./hooks/Hooks";
@@ -38,50 +37,41 @@ const App = () => {
         background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
       }}
     >
-      <BrowserRouter>
-        <AdoptedPetContext.Provider value={adoptedPet}>
-          <QueryClientProvider client={queryClient}>
-            <Suspense
-              fallback={
-                <div className="loading-pane">
-                  <h2 className="loader">🐶</h2>
-                </div>
-              }
-            >
-              <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
-                <Link
-                  className="text-6xl text-white hover:text-gray-200"
-                  to="/"
-                >
-                  Adopt Me!
-                </Link>
-              </header>
-              <nav>
-                <Link to="/hooks">
-                  Hooks Examples from Intermediate React v5
-                </Link>
-              </nav>
-              <Routes>
-                <Route path="/hooks" element={<Hooks />}>
-                  <Route path="" element={<Home />} />
-                  <Route path="useReducer" element={<UseReducer />} />
-                  <Route path="useMemo" element={<UseMemo />} />
-                  <Route path="useCallback" element={<UseCallback />} />
-                  <Route path="useRef" element={<UseRef />} />
-                  <Route path="useLayoutEffect" element={<UseLayoutEffect />} />
-                  <Route path="useId" element={<UseId />} />
-                </Route>
-                <Route path="/details/:id" element={<Details />} />
-                <Route path="/" element={<SearchParams />} />
-              </Routes>
-            </Suspense>
-          </QueryClientProvider>
-        </AdoptedPetContext.Provider>
-      </BrowserRouter>
+      <AdoptedPetContext.Provider value={adoptedPet}>
+        <QueryClientProvider client={queryClient}>
+          <Suspense
+            fallback={
+              <div className="loading-pane">
+                <h2 className="loader">🐶</h2>
+              </div>
+            }
+          >
+            <header className="mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center">
+              <Link className="text-6xl text-white hover:text-gray-200" to="/">
+                Adopt Me!
+              </Link>
+            </header>
+            <nav>
+              <Link to="/hooks">Hooks Examples from Intermediate React v5</Link>
+            </nav>
+            <Routes>
+              <Route path="/hooks" element={<Hooks />}>
+                <Route path="" element={<Home />} />
+                <Route path="useReducer" element={<UseReducer />} />
+                <Route path="useMemo" element={<UseMemo />} />
+                <Route path="useCallback" element={<UseCallback />} />
+                <Route path="useRef" element={<UseRef />} />
+                <Route path="useLayoutEffect" element={<UseLayoutEffect />} />
+                <Route path="useId" element={<UseId />} />
+              </Route>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
+        </QueryClientProvider>
+      </AdoptedPetContext.Provider>
     </div>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
