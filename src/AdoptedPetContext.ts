2ae8425d05678fa6eb7ba8489137f2849d29f117
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-12-05 23:15:10 -0800

    Fix types in AdoptedPetContext.ts

--- src/AdoptedPetContext.ts
+++ src/AdoptedPetContext.ts
@@ -1,5 +1,18 @@
 import { createContext } from "react";
+import { Pet } from "./APIResponsesTypes";
 
-const AdoptedPetContext = createContext();
+const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void]>([
+  {
+    id: 42,
+    name: "Fido",
+    animal: "dog",
+    description: "Lorem ipsum",
+    breed: "Beagle",
+    images: [],
+    city: "Seattle",
+    state: "WA",
+  },
+  () => {},
+]);
 
 export default AdoptedPetContext;
