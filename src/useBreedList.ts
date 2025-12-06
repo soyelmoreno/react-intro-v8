191f12ca57aefab68cf9d8d18db6d126aae3f804
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-12-06 00:56:27 -0800

    Fix types for useBreedList

--- src/useBreedList.ts
+++ src/useBreedList.ts
@@ -1,8 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryStatus, useQuery } from "@tanstack/react-query";
 import fetchBreedList from "./fetchBreedList";
+import { Animal } from "./APIResponsesTypes";
 
-export default function useBreedList(animal) {
+export default function useBreedList(animal: Animal) {
   const results = useQuery(["breeds", animal], fetchBreedList);
 
-  return [results?.data?.breeds ?? [], results.status];
+  return [results?.data?.breeds ?? [], results.status] as [
+    string[],
+    QueryStatus
+  ];
 }
